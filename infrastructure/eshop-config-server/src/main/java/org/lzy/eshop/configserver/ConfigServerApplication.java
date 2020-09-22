ab89629d6527697fc146e2d28c3eb3f851b0539f
liuziyuan <liuziyuan.room@gmail.com>
2020-09-22 15:42:58 +0800

    add logback and config server project support logstash

--- infrastructure/eshop-config-server/src/main/java/org/lzy/eshop/configserver/ConfigServerApplication.java
+++ infrastructure/eshop-config-server/src/main/java/org/lzy/eshop/configserver/ConfigServerApplication.java
@@ -1,11 +1,13 @@
 package org.lzy.eshop.configserver;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+@Slf4j
 @EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
